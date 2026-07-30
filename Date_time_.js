e0ad92484a74d2aebc7f1425f90443595cdc0c7a
SUDARSINI  A G <162299269+SUDARSINI2156@users.noreply.github.com>
2026-07-30 15:43:29 +0530

    Add files via upload

---
+++ Date_time_.js
@@ -0,0 +1,8 @@
+let bday = new Date('2026-05-21');
+//if nothing given as argument mean return current date
+console.log(bday.getDate());//date
+console.log(bday.getDay());//day ooda number
+console.log(bday.getFullYear());//yaer
+console.log(bday.getMonth());
+
+
