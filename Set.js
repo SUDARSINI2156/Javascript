a811f60ac5ef479b68a11c5701e54118ac3d933d
SUDARSINI  A G <162299269+SUDARSINI2156@users.noreply.github.com>
2026-08-06 16:09:41 +0530

    Add files via upload

---
+++ Set.js
@@ -0,0 +1,22 @@
+let set = new Set();
+
+set.add('codeio')
+set.add('logic.io')
+//.add('codeio')
+console.log(set);
+//we can add any datatype
+//duplicate not allow
+
+//delete
+
+set.clear();
+console.log(set);
+
+//size
+console.log(set.size);
+
+//for loop
+
+for(s of set){
+    console.log(s);
+}
---
+++ Set_practice.js
@@ -0,0 +1,13 @@
+let set = new Set();
+
+set.add(7)
+.add(3)
+.add(3)
+.add(9)
+.add(6)
+.add(7)
+.add(5)
+.add(8)
+.add(2)
+.add(7)
+console.log(set.size);
